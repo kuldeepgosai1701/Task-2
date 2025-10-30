1438345af475fa4a65e23046f4e50b64e421b1ac
Kuldeep Gosai <kuldeep.goswami@raoinformationtechnology.com>
2025-10-30 12:00:34 +0530

    new change

---
+++ main.js
@@ -0,0 +1,36 @@
+
+  @param {object} sessionData 
+
+function sendToGoogleSheet(sessionData) {
+    
+    const WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbzJAlgY04Kbz2fj6sZ-wqR6bHJaJaDLhm73Qh4XKI_ayTq2TPwL2w-Hd3h2MMsyj8_Z/exec'; 
+
+    fetch(WEB_APP_URL, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
+        },
+        
+        body: JSON.stringify({
+            startPoint: `${sessionData.startLat}, ${sessionData.startLon}`,
+            endPoint: `${sessionData.endLat}, ${sessionData.endLon}`,
+            startTime: sessionData.startTime.toLocaleString(), // Format time nicely
+            endTime: sessionData.endTime.toLocaleString()
+        })
+    })
+    .then(response => {
+        return response.json(); 
+    })
+    .then(data => {
+        console.log('Success sending data:', data);
+        // Add a check to confirm the 'result' is 'success'
+        if (data.result === 'success') {
+             console.log('Data successfully logged to Google Sheet.');
+        } else {
+             console.error('Apps Script reported an error:', data.message);
+        }
+    })
+    .catch((error) => {
+        console.error('Error sending data to Google Sheet:', error);
+    });
+}
